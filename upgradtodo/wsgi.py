8517148e3842f130733b69d6398a930e8efaccc2
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2019-03-14 20:00:37 +0530

    Init

---
+++ upgradtodo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for upgradtodo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'upgradtodo.settings')
+
+application = get_wsgi_application()
